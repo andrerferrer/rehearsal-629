1f271ec6f69b628d12767dc79e578bf0615725fc
andre ferrer <andrerferrer@hotmail.com>
2021-02-10 07:27:54 -0300

    Revert mistake

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-// 1st select the element
-const btn = document.querySelector('#click-me');
-// console.log(btn);
-
-// 2nd add event listener
-// btn.addEventListener('event', 'behavior')
-btn.addEventListener('click', (event) => {
-  // console.log(event); // click ITSELF
-  // console.log(event.currentTarget); // element that we clicked on
-  // btn == event.currentTarget
-  
-  // 3rd do the behavior - changing the inner text of the btn
-  event.currentTarget.innerText = 'Warten Sie bitte...';
-  btn.disabled = true;
-});
-
+console.log("Hello from src/index.js!");
