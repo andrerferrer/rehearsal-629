772d3927e638ee5309178855139b2b886c157b29
andre ferrer <andrerferrer@hotmail.com>
2021-05-26 06:03:20 -0300

    finish lovecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello from src/index.js!");
+
+// select the element
+const btn = document.querySelector('#click-me');
+
+// do the callback
+const darkModeOn = (event) => {
+  // console.log(event);
+  const body = document.querySelector('body');
+  body.classList.toggle('dark-mode');
+};
+
+// add event listener
+btn.addEventListener('click', darkModeOn);
